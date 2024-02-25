8d21d8b1a04a8a49403ae29028416ba1cbfa996e
Saksham Srivastava <96429260+Saksham2716@users.noreply.github.com>
2024-02-25 16:38:40 +0000

    strings datatype use

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name="Saksham"
+const age=22
+//console.log(name + age + "best") {older format}
+console.log(`Hello, My name is ${name}, and age is ${age}` ); //{newer format}
+const NameGame= new String("Sakshamplay")
+console.log(NameGame[0])
+console.log(NameGame.__proto__)
+console.log(NameGame.length)
+console.log(NameGame.toLocaleUpperCase())
+console.log(NameGame.charAt(3))
