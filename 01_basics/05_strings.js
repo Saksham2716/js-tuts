00250fcd0d75ba2b6a632a6132ada1f420c8b732
Saksham Srivastava <96429260+Saksham2716@users.noreply.github.com>
2024-02-27 18:28:50 +0000

    strings in js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,9 +2,27 @@ const name="Saksham"
 const age=22
 //console.log(name + age + "best") {older format}
 console.log(`Hello, My name is ${name}, and age is ${age}` ); //{newer format}
-const NameGame= new String("Sakshamplay")
+const NameGame= new String("Saksham-play")
 console.log(NameGame[0])
 console.log(NameGame.__proto__)
 console.log(NameGame.length)
 console.log(NameGame.toLocaleUpperCase())
 console.log(NameGame.charAt(3))
+console.log(NameGame.indexOf('p'))
+
+const newString = NameGame.substring(0,4)
+console.log(newString)
+
+const anotherString =  NameGame.slice(-11,4)
+console.log(anotherString)
+
+const newstringone = "    Hey Saksham   "
+console.log(newstringone.trim())
+
+const url = "https://developer.mozilla.org/en-US/"
+
+console.log(url.replace('en', 'in'))
+
+console.log(url.includes('sundar'))
+
+console.log(NameGame.split('-'))
